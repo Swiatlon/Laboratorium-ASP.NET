52f514006a5c83e8395457a9a764ea6fc4e0aa3a
unknown <wiercikdoc@gmail.com>
2023-12-05 16:30:09 +0100

    Created other folder for Lab5

---
+++ Laboratorium_5/Models/ProductModel.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Laboratorium_5.Models
+{
+    public class ProductModel
+    {
+        [HiddenInput]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name is required.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Price is required.")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Price must be greater than 0.")]
+        public decimal Price { get; set; }
+
+        [Required(ErrorMessage = "Producer is required.")]
+        public string Producer { get; set; }
+
+        [Required(ErrorMessage = "Production date is required.")]
+        [DataType(DataType.Date, ErrorMessage = "Enter a valid production date.")]
+        public DateTime ProductionDate { get; set; }
+
+        [Required(ErrorMessage = "Description is required.")]
+        [StringLength(1000, MinimumLength = 10, ErrorMessage = "Description must be between 10 and 1000 characters.")]
+        public string Description { get; set; }
+    }
+}
---
+++ Laboratorium_5/Program.cs
@@ -0,0 +1,38 @@
+namespace Laboratorium_5
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddDbContext<Data.AppDbContext>();
+            builder.Services.AddTransient<IProductService, EFProductService>();
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
